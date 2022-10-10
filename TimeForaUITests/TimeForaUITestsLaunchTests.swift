d4212b00648f20fb1236d6359bd812d1dc8cfa43
CapistranoIFCE <101426741+CapistranoIFCE@users.noreply.github.com>
2022-10-10 14:33:26 -0300

    Initial Commit

---
+++ TimeForaUITests/TimeForaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TimeForaUITestsLaunchTests.swift
+//  TimeForaUITests
+//
+//  Created by Davi Capistrano on 10/10/22.
+//
+
+import XCTest
+
+final class TimeForaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
